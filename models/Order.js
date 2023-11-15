263daa9bf472ec08118f4c9fda5ee2d53e1ef814
Kirsi Mak <ying987554@hotmail.com>
2023-11-16 02:44:30 +0800

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -3,15 +3,13 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const orderSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  orderId: { type: String, required: true },
+  orderId: { type: String, required: true, unique: true },
   date: { type: String, required: true },
   product: [{
     storeId: { type: String, required: true },
     productId: { type: String, required: true },
     name: { type: String, required: true },
-    img: [{
-      img: { type: String }
-    }],
+    img: { type:[{type:Buffer}]},
     price: { type: Number, required: true, $gte: 0 },
     info: { type: String },
     qty: { type: Number, required: true, $gte: 0 }

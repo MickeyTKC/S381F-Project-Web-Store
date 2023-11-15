e1fbf64fdea4a9d9b757dd49359ca209fdbaaabf
MickeyTKC <tt51145505@gmail.com>
2023-11-15 19:31:16 +0800

    Create User model

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["client", "operator", "admin"], required: true },
+  name: { type: String, required: true },
+  description: { type: String },
+  address: { type: String },
+});
+
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("User",userSchema)

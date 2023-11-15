0c07b32c7d3c376474eb596885229a7b39ff7f35
MickeyTKC <tt51145505@gmail.com>
2023-11-16 00:22:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser")
 const passport = require("passport")
 const ejs = require("ejs")
+const mongoose = require("mongoose")
 
 const express = require("express");
 const session = require("express-session");
@@ -8,11 +9,18 @@ const app = express();
 
 const port = 3000;
 const secert = "key";
-const routes = require("./rotues");
+const routes = require("./routes");
+
+const dbName = "SSProject"
+const url = `mongodb+srv://serverSide_User:serrrrver_side1@cluster0.eknv0ni.mongodb.net/${dbName}`;
+mongoose.set("strictQuery",true)
+mongoose.connect(url).then(()=>{
+  console.log("mongoDB connected successfully")
+})
 
 //setup view engine
 app.set("view engine", "ejs");
-app.use(bodyParser)
+//app.use(bodyParser)
 //setup session
 app.use(
   session({

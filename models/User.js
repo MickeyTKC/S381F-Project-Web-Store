efe746ea18aaed6d41a7025c06fd68901e63b1a0
Kirsi Mak <ying987554@hotmail.com>
2023-11-15 23:32:49 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true },
+  userId: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["client", "operator", "admin"], required: true },
   name: { type: String, required: true },

e9d7e81ce492fbd3f56ff120848a3e8eb6d258dd
MickeyTKC <tt51145505@gmail.com>
2023-11-15 18:32:34 +0800

    create routes

---
+++ rotues/product.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Define a route for the root URL ("/")
+router.get('/', (req, res) => {
+  const data = {
+    message: 'Product',
+    timestamp: new Date()
+  };
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data));
+});
+
+// Start the server
+module.exports = router;

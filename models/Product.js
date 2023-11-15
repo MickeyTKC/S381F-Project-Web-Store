e666f8249ecb91f9781572c662583c7033422822
Kirsi Mak <ying987554@hotmail.com>
2023-11-16 02:44:42 +0800

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -3,17 +3,13 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const productSchema = new mongoose.Schema({
   storeId: { type: String, required: true },
-  productId: { type: String, required: true },
+  productId: { type: String, required: true, unique: true},
   name: { type: String, required: true },
-  img: [{
-    img: { type: String }
-  }],
+  img: { type:[{type:Buffer}]},
   price: { type: Number, required: true, $gte: 0 },
   info: { type: String },
   stock: { type: Number, required: true, $gte: 0 },
-  tags: [{
-    tags: { type: String }
-  }]
+  tags: { type:[{type:String}]}
 })
 
 productSchema.plugin(passportLocalMongoose);

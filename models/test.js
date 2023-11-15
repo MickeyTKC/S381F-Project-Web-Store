e35dbf3fc46616067139a2dc6e1eb15d1baef16a
MickeyTKC <tt51145505@gmail.com>
2023-11-16 00:22:08 +0800

    Update test.js

--- models/test.js
+++ models/test.js
@@ -8,20 +8,11 @@ mongoose.set("strictQuery",true)
 
 
 mongoose.connect(url)
-console.log(mongoose.connection.readyState);
 
 const run = async () =>{
-    var createUser = await User.create({
-        id:"root",
-        password:"root",
-        role:"admin",
-        name:"Mickey",
-        description:"Bad Guy",
-        address:"HK",
-    })
-    console.log(createUser)
     var users = await User.find();
     console.log(users)
 }
+
 run();
 console.log("finished")

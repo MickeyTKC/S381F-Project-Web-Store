4aaf43eab7c2d1c7e02e32e461bc41905b9437a3
MickeyTKC <tt51145505@gmail.com>
2023-11-16 01:41:05 +0800

    Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
+const session = require("express-session");
 
 const User = require("../models/User");
-
-/** 
-const passport = require("passport");
-router.use(User.createStrategy());
-
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-  User.findOne({ id: id }, function () {
-    done(err, user);
-  });
-});
-*/
 
 // Define a route for the root URL ("/")
 router.get("/", (req, res) => {
@@ -33,15 +19,39 @@ router.get("/login", (req, res) => {
   res.render("../views/login");
 });
 
-router.post("/login", (req, res) => {
-  console.log(req.query)
-  console.log(req.params)
-  const user = {} // User.find({ userId: id });
+router.post("/login", async (req, res) => {
+  const id = req.body.userId;
+  const pw = req.body.password;
   res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify(user));
+
+  const user = await User.findOne({ userId: id });
+  const message = {};
+  if (!user) {
+    message.err = "User does not exist.";
+    res.send(JSON.stringify(message));
+    res.redirect("/login");
+  }
+  if (user.password != pw) {
+    message.err = "The password is incorrect.";
+    res.send(JSON.stringify(message));
+    res.redirect("/login");
+  }
+  if (user.password == pw) {
+    message.msg = "Login successfully";
+    res.send(JSON.stringify(message));
+    req.session.authenticated = true;
+    req.session.user = user;
+    req.session.save()
+  }
 });
 
 router.post("/signup", (req, res) => {});
+
+router.post("/logout",(req,res)=>{
+  req.session.destroy(() => {
+    console.log('session destroyed')
+  })
+})
 
 // Start the server
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const bodyParser = require("body-parser")
-const passport = require("passport")
-const ejs = require("ejs")
-const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const express = require("express");
 const session = require("express-session");
@@ -11,29 +11,49 @@ const port = 3000;
 const secert = "key";
 const routes = require("./routes");
 
-const dbName = "SSProject"
+const dbName = "SSProject";
 const url = `mongodb+srv://serverSide_User:serrrrver_side1@cluster0.eknv0ni.mongodb.net/${dbName}`;
-mongoose.set("strictQuery",true)
-mongoose.connect(url).then(()=>{
-  console.log("mongoDB connected successfully")
-})
+mongoose.set("strictQuery", true);
+mongoose.connect(url).then(() => {
+  console.log("mongoDB connected successfully");
+});
 
 //setup view engine
 app.set("view engine", "ejs");
-//app.use(bodyParser)
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //setup session
 app.use(
   session({
     secret: secert,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
   })
 );
-//init passport
-app.use(passport.initialize())
-//use passport deal with session
-app.use(passport.session())
 
+const auth = (req, res, next) => {
+  if (req.session.user) {
+    console.log("authenticated");
+    console.log(`userId:${req.session.user.id},role:${req.session.user.role}`);
+    next();
+  } else {
+    console.log("not authenticated");
+    return res.redirect("/");
+  }
+};
+
+app.use((req, res, next) => {
+  if (req.session.user) {
+    const user = req.session.user;
+    console.log(
+      `userId:${user.userId},passowrd:${user.password},role:${user.role}`
+    );
+  }
+  next();
+});
 
 app.get("/", (req, res) => {
   const data = {

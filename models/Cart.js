50ff38b665e8699355cee24f8547a1a5eff4b138
Kirsi Mak <ying987554@hotmail.com>
2023-11-15 23:45:22 +0800

    Create Cart.js

---
+++ models/Cart.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const cartSchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  product: [{
+    storeId: { type: String, required: true },
+    productId: { type: String, required: true },
+    name: { type: String, required: true },
+    img: [{
+      img: { type: String }
+    }],
+    price: { type: Number, required: true, $gte: 0 },
+    info: { type: String },
+    qty: { type: Number, required: true, $gte: 0 }
+  }]
+});
+
+cartSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("Cart", cartSchema);

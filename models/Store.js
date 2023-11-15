196e2e5c061d051d44389bb4c92bab8ce8293db0
Kirsi Mak <ying987554@hotmail.com>
2023-11-16 01:11:10 +0800

    Update storeSchema

--- models/Store.js
+++ models/Store.js
@@ -3,9 +3,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const storeSchema = new mongoose.Schema({
   storeId: { type: String, required: true },
-  userId: [{
-    userId: { type: String, required: true }
-  }],
+  userId: { type:[{type:String, required: true}]},
   name: { type: String, required: true },
   img: { type: String }, //store logo
   info: { type: String },

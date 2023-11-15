5f4fe0614ccbba1ab5dc82fefd7690e0c313f302
Kirsi Mak <ying987554@hotmail.com>
2023-11-16 02:44:20 +0800

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const cartSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, required: true, unique: true},
   product: [{
     storeId: { type: String, required: true },
     productId: { type: String, required: true },
     name: { type: String, required: true },
-    img: [{
-      img: { type: String }
-    }],
+    img: { type:[{type:Buffer}]},
     price: { type: Number, required: true, $gte: 0 },
     info: { type: String },
     qty: { type: Number, required: true, $gte: 0 }

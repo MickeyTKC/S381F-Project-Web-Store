60b941bba633b94435de21d39810a250fa40feec
Kirsi Mak <ying987554@hotmail.com>
2023-11-17 02:13:20 +0800

    Update StoreOrder.js

--- models/StoreOrder.js
+++ models/StoreOrder.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const storeOrderSchema = new mongoose.Schema({
   storeId: {type: String, required: true},
   orderId: { type: String, required: true, unique: true },
-  productId: { type: String, required: true },
-  userId: { type: String, required: true },
-  address: {type: String},
-  phoneNo: {type: String},
-  date: { type: String, required: true },
-  qty: { type: Number, required: true, $gte: 0 }
+  date: { type: String, required: true }, 
+  product:{
+    productId: { type: String, required: true },
+    name: { type: String, required: true },
+    qty: { type: Number, required: true, $gte: 0 }
+  }, 
+  user: {
+    userId: { type: String, required: true },
+    address: {type: String},
+    phoneNo: {type: String}
+  }
 })
 
 module.exports = mongoose.model("StoreOrder", storeOrderSchema); 

fd48dd59af2d2584e0b2e304c793894a936d65b7
Kirsi Mak <ying987554@hotmail.com>
2023-11-17 02:49:07 +0800

    find func for Product

--- models/Product.js
+++ models/Product.js
@@ -11,4 +11,38 @@ const productSchema = new mongoose.Schema({
   tags: { type:[{type:String}]}
 })
 
+//------------------Function start------------------
+
+productSchema.statics.findBystoreId = function (storeId){  //find by storeId
+  return this.find({storeId: storeId});
+}
+
+productSchema.statics.findByName = function (name){  //find by name
+  return this.find({name: {$regex: name}});
+}
+
+productSchema.statics.findByInfo = function (info){  //find by info
+  return this.find({info: {$regex: info}});
+}
+
+productSchema.statics.findByTag = function (tag){ //find by tag
+  return this.find({tags: {$elemMatch:{$in: tag}}});
+}
+
+productSchema.statics.findByPriceLower = function (price){ //find lower by price
+  return this.find({price: {$lte : price}});
+}
+
+productSchema.statics.findByPriceGreater = function (price){ //find greater by price
+  return this.find({price: {$gte : price}});
+}
+
+productSchema.statics.findByStockLow = function (number){ //find stock which is low
+  return this.find({stock: {$lte: number}});
+}
+
+// productSchema.statics.filter = function (Product){ //filter
+//   return this.find();
+// }
+
 module.exports = mongoose.model("Product", productSchema);

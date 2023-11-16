a33cbb09400f4bd96beb27e4d7374e19201157eb
MickeyTKC <tt51145505@gmail.com>
2023-11-16 20:00:19 +0800

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Define a route for the root URL ("/")
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const data = {
-    message: 'Product',
-    timestamp: new Date()
+    message: "Product",
+    timestamp: new Date(),
   };
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(data));
+});
+
+router.get("/id/:id", (req, res) => {
+  const data = {
+    message: "Product",
+    timestamp: new Date(),
+  };
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify(data));
 });
 

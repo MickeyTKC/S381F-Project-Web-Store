541182d8b151b457b5f65b98fd04e420ea77323b
Kirsi Mak <ying987554@hotmail.com>
2023-11-16 23:55:32 +0800

    Find func for User

--- models/User.js
+++ models/User.js
@@ -11,4 +11,23 @@ const userSchema = new mongoose.Schema({
   phoneNo: {type:String}
 });
 
+//------------------Function start------------------
+
+userSchema.statics.findByIdE = function (userId, email){  //find by userId, email
+  return this.find({userId: userId, email: email});
+}
+
+userSchema.statics.findByIdP = function (userId, password){  //find by userId, password
+  return this.find({userId: userId, password: password});
+}
+
+userSchema.statics.findByEP = function (email, password){  //find by email, password
+  return this.find({email: email, password: password});
+}
+
+userSchema.statics.findByNAP = function (name, address, phoneNo){ //finc by name, address, phoneNo
+  return this.find({name: name, address: address, phoneNo: phoneNo});
+}
+
+
 module.exports = mongoose.model("User",userSchema)

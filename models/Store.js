1838f42de44b8581e80c38b90aa1c5feabb2a6c6
Kirsi Mak <ying987554@hotmail.com>
2023-11-17 02:57:12 +0800

    find func for Store

--- models/Store.js
+++ models/Store.js
@@ -9,4 +9,26 @@ const storeSchema = new mongoose.Schema({
   address: { type: String, required: true }
 });
 
+//------------------Function start------------------
+
+storeSchema.statics.findByStoreId = function (storeId){  //find by storeId
+  return this.find({storeId: storeId});
+}
+
+storeSchema.statics.findByUserId = function (userId){  //find by userId
+  return this.find({userId: userId});
+}
+
+storeSchema.statics.findByName = function (name){  //find by name
+  return this.find({name: {$regex: name}});
+}
+
+storeSchema.statics.findByInfo = function (info){  //find by info
+  return this.find({info: {$regex: info}});
+}
+
+storeSchema.statics.findByAddress = function (address){ //find by address
+  return this.find({address: {$regex: address}});
+}
+
 module.exports = mongoose.model("Store", storeSchema);

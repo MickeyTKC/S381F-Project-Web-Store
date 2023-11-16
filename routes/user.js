5e2632a4f4b94f20366b6e61ab127d342f003b47
MickeyTKC <tt51145505@gmail.com>
2023-11-16 22:19:14 +0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User")
 
 // Define a route for the root URL ("/")
-router.get('/', (req, res) => {
-  const data = {
-    message: 'User',
-    timestamp: new Date()
-  };
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(data));
+const auth = (req, res, next) => {
+  if (!req.session.user){
+    throw new Error("Permission Required")
+  }
+  if (req.session.user.role!="admin"){
+    throw new Error("Admin Permission Required")
+  }
+  next();
+};
+
+router.get('/', auth, async (req, res) => {
+  const contentType = req.header("content-type");
+  const users = await User.find();
+  console.log(users)
+  if (contentType == "application/json") {
+    res.send(JSON.stringify(users));
+  }else{
+    res.render("../views/users.ejs",{users:users})
+  }
 });
+
+router.use((err, req, res, next)=>{
+  console.log(err)
+  return res.status(400).send(err.message)
+})
 
 // Start the server
 module.exports = router;

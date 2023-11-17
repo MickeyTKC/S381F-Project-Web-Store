a99ffacb71c6ba2ae684a2b836324ad8fd421995
Kirsi Mak <ying987554@hotmail.com>
2023-11-18 02:56:54 +0800

    Update store.js

--- routes/store.js
+++ routes/store.js
@@ -1,57 +1,63 @@
 const express = require("express");
 const router = express.Router();
+const fileupload = require("express-fileupload");
 
 const Product = require("../models/Product")
 const Store = require("../models/Store")
 
+app.use(fileupload());
 const authAdmin = (req, res, next) => {
   const err = {};
   if (!req.session.user) {
     err.statusCode = 403
-    err.message("Permission Required")
+    err.message="Permission Required";
     next(err)
   }
   if (req.session.user.role != "admin") {
     err.statusCode = 403
-    err.message("Admin Permission Required")
+    err.message="Admin Permission Required";
     next(err)
   }
   next();
 };
 
 const authOperator = (req, res, next) => {
+  const err = {};
   if (!req.session.user) {
-    throw new Error("Permission Required");
+    err.statusCode = 403
+    err.message("Permission Required");
+    next(err)
   }
-  if (req.session.user.role != "operator" || req.session.user.role != "admin") {
-    throw new Error("Admin/Operator Permission Required");
+  if (req.session.user.role != "operator") {
+    err.statusCode = 403
+    err.message("Admin/Operator Permission Required");
+    next(err)
   }
   next();
 };
 
-// get store request for admin views all store
-router.get("/", authAdmin, (req, res) => {
-  const data = {
-    message: "Store",
-    timestamp: new Date(),
-  };
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify(data));
-});
 
 // view store details
-router.get("id/:id", (req, res) => {});
-
-// create a new store
-router.post("/",authAdmin,(req, res) =>{
-
-})
+router.get("/", (req, res) => {});
 
 // edit store information 
-router.put("/",authOperator,(req, res) =>{
-  //check operator
+router.put("/",authAdmin, async (req, res) =>{
+    
     //store update
-})
+    const contentType = req.header("content-type");
+    const err = {};
+    // get data
+    const storeData = await Store.findOne({});
+    const store = {
+      name: req.body.name,
+      img: req.body.img, //store logo
+      info: req.body.info,
+      address: req.body.address
+    };
+    console.log(store);
+    ///await Store.findOneAndUpdate({productId:req.body.product},product);
+    await Store.updateOne(storeData.storeId, store);
+});
 
 // create a new store product
 router.post("/product",authOperator,(req, res) =>{

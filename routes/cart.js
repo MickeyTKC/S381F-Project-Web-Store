36fd4320a6f2e3b93fdb27dc8195f3328d99bed1
MickeyTKC <tt51145505@gmail.com>
2023-11-18 06:20:34 +0800

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const Cart = require("../models/Cart");
+const Product = require("../models/Product");
 
 // the auth for Login required
 const auth = (req, res, next) => {
   if (!req.session.user) {
-    err.message ="Login Required";
+    err.message = "Login Required";
     next(err);
   }
   next();
@@ -17,13 +18,18 @@ router.get("/", auth, async (req, res) => {
   const userId = req.session.user.userId;
   const contentType = req.header("content-type");
   const myCart = await Cart.findByUserId(userId);
-  const myCartImg = Buffer.from(myCart.product.img, 'base64');
-  myCart.product.img = myCartImg;
+  if (myCart) {
+    for (p of myCart.product) {
+      var myCartImg;
+      if (p.img) myCartImg = new Buffer(p.img, "base64").toString();
+      p.img = myCartImg ? `data:image/jpg;base64,${myCartImg}` : "/noImage.jpg";
+    }
+  }
   if (contentType == "application/json") {
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(data));
   } else {
-    res.render("../views/cart.ejs", { cart: myCart });
+    res.render("../views/cart.ejs", { user: req.session.user, cart: myCart });
   }
 });
 
@@ -31,19 +37,57 @@ router.get("/", auth, async (req, res) => {
 router.post("/", auth, async (req, res) => {
   const userId = req.session.user.userId;
   const contentType = req.header("content-type");
-  console.log("adding porduct to cart")
+  console.log("adding porduct to cart");
 });
 
-router.put("/", auth, async (req, res) => {
-  console.log("edit cart porduct qty")
-})
+router.put("/producId/:id", auth, async (req, res, next) => {
+  const userId = req.session.user.userId;
+  const productOfCart = req.params.id;
+  const product = await Product.findByProductId(productOfCart);
+  try {
+    console.log(
+      `put product ${product.productId}-${product.name} from User:${userId} Name:${req.session.user.name}`
+    );
+    const newProduct = {
+      productId: product.productId,
+      name: product.name,
+      img: "",
+      price: product.price,
+      qty: 1
+    };
+    console.log(newProduct)
+    const pushProduct = await Cart.updateOne(
+      { userId: userId },
+      { $push: { product: newProduct } }
+    );
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/producId/:id", auth, async (req, res, next) => {
+  const userId = req.session.user.userId;
+  const productOfCart = req.params.id;
+  console.log(
+    `delete porduct ${req.params.id} from User:${userId} Name:${req.session.user.name}`
+  );
+  try {
+    const cartProduct = await Cart.updateOne(
+      { userId: userId },
+      { $pull: { product: { productId: productOfCart } } }
+    );
+  } catch (e) {
+    next(e);
+  }
+  res.redirect("/cart");
+});
 
 router.use((err, req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   // Default error status code
   const statusCode = err.statusCode || 500;
   // Default error message
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   // Send error response
   res.status(statusCode).json({ error: message });
 });

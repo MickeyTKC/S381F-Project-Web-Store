f58fa71bc31b524bdc44ea6296195480061a695f
Kirsi Mak <ying987554@hotmail.com>
2023-11-17 23:20:16 +0800

    Update Store.js

--- models/Store.js
+++ models/Store.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const storeSchema = new mongoose.Schema({
   storeId: { type: String, required: true, unique: true},
-  userId: { type:[{type:String, required: true}]},
   name: { type: String, required: true },
   img: { type: Buffer }, //store logo
   info: { type: String },
@@ -11,24 +10,5 @@ const storeSchema = new mongoose.Schema({
 
 //------------------Function start------------------
 
-storeSchema.statics.findByStoreId = function (storeId){  //find by storeId
-  return this.findOne({storeId: storeId});
-}
-
-storeSchema.statics.findByUserId = function (userId){  //find by userId
-  return this.find({userId: {$elemMatch:userId}});
-}
-
-storeSchema.statics.findByName = function (name){  //find by name
-  return this.find({name: {$regex: name}});
-}
-
-storeSchema.statics.findByInfo = function (info){  //find by info
-  return this.find({info: {$regex: info}});
-}
-
-storeSchema.statics.findByAddress = function (address){ //find by address
-  return this.find({address: {$regex: address}});
-}
 
 module.exports = mongoose.model("Store", storeSchema);

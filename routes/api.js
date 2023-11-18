d7e78b3a4ab5541aad827c4ffb6fc9a6e4d774c5
MickeyTKC <tt51145505@gmail.com>
2023-11-18 18:08:53 +0800

    Create api.js

---
+++ routes/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/User");
+const Store = require("../models/Store");
+const Order = require("../models/Order");
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
+
+
+
+module.exports = router;

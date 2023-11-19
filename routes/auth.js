4bd2a9dc1d76b29ac608a640d3bb5ce9e950a22e
MickeyTKC <tt51145505@gmail.com>
2023-11-20 00:34:29 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,15 +7,17 @@ router.post("/login", async (req, res, next) => {
   const id = req.body.userId;
   const pw = req.body.password;
   const user = (await User.findByUserId(id)) || {};
-  const err = {};
-  if (!user) next({ statusCode: 400, message: "User does not exist." });
+  if (!user)
+    if (req.header("content-type") == "application/json")
+      return next({ statusCode: 400, message: "User does not exist." });
   if (user.password != pw)
-    return next({ statusCode: 400, message: "The password is incorrect." });
+    if (req.header("content-type") == "application/json")
+      return next({ statusCode: 400, message: "The password is incorrect." });
   if (user.password == pw) {
     // setup express session
     req.session.authenticated = true;
     req.session.user = user;
-    if (!req.header("content-type") == "application/json") {
+    if (req.header("content-type") == "application/json") {
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(req.session);
     } else {
@@ -26,13 +28,13 @@ router.post("/login", async (req, res, next) => {
 
 router.post("/logout", (req, res, next) => {
   const sessionUser = req.session.user;
-  if (!sessionUser)
-    return next({
-      statusCode: 401,
-      message: "Unauthorized, session is empty.",
-    });
   req.session = null;
   if (!req.header("content-type") == "application/json") {
+    if (!sessionUser)
+      return next({
+        statusCode: 401,
+        message: "Unauthorized, session is empty.",
+      });
     res.status(200).json({ message: "success" });
   } else {
     res.redirect("/");
